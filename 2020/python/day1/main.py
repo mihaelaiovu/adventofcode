c392aea65fef5e4d6397461b71216dc6bc911fb2
mihaelaiovu <iovu.mihaela@hotmail.com>
2020-12-03 18:29:07 +0100

    Corrected the mistake, i to i+1

--- 2020/python/day1/main.py
+++ 2020/python/day1/main.py
@@ -3,12 +3,12 @@ with open("input.txt", 'r') as f:
     x = [int(x.strip()) for x in content]
 
 for i in range(len(x)):
-    for j in range(i, len(x)):
+    for j in range(i + 1, len(x)):
         if x[i] + x[j] == 2020:
             print(x[i] * x[j])
 
 for i in range(len(x)):
-    for j in range(i, len(x)):
+    for j in range(i + 1, len(x)):
         for k in range(j, len(x)):
             if x[i] + x[j] + x[k] == 2020:
                 print(x[i] * x[j] * x[k])
